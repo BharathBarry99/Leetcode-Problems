e1626b499f7b41877e6652f7ab266ad194e5516d
Bharathraj S <bharathrajsaravanan@gmail.com>
2023-02-04 11:55:04 +0530

    modified Remove_duplicates_from_sorted_array.cpp

--- Arrays/Remove_duplicates_from_sorted_array.cpp
+++ Arrays/Remove_duplicates_from_sorted_array.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-    vector<int> todel;
-        set<int> setu;
-        int count=0;
+
+        int ind=0;
         int n=nums.size();
+
         if (nums.empty()) return 0;
         if (nums.size()==1){
             return 1;
         }
-        for (const auto &ele : nums){
-            setu.insert(ele);
-        }
-        for(int i =0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                nums[i]=INT_MAX;
-            }
-        }
-        sort(nums.begin(),nums.end());
 
         
-     return setu.size();  
+        for(int i = 1; i<n;i++){
+            if(nums[ind]!=nums[i]){
+                ind++;
+                nums[ind]=nums[i];
+            }
+        }
+
+     return ind+1;  
     }
 };
+// The time complexity of the removeDuplicates function is O(n), where n is the number of elements in the vector nums. 
+// The function uses two pointers, one to keep track of unique elements and the other to traverse the vector. As a result, the function performs a constant amount of work for each element in the vector and thus has a linear time complexity.
+
+// The space complexity of the function is O(1), as the function only uses a few variables to store intermediate values and does not require any additional data structures.
+// The function modifies the input vector in-place, so no additional memory is allocated to store the results.
